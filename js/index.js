2ad4d160b8cdc0d1a4c2b16cff260468fae29a88
SarahAvery <70301632+SarahAvery@users.noreply.github.com>
2021-05-06 16:02:51 -0400

    Updates

--- js/index.js
+++ js/index.js
@@ -40,7 +40,7 @@
         var isDark = $("body").hasClass("dark-theme");
         localStorage.setItem("theme", isDark ? "dark" : "");
       });
-    }, 100); // Image on click full size if screen is more than 500px
+    }, 300); // Image on click full size if screen is more than 500px
 
     $(window).on("load resize", function () {
       var width = $(window).width();
